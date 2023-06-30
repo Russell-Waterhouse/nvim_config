a011a232b4c5a3bcd6278037ad5ab3d84985d07f
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2023-06-29 22:35:47 -0700

    add remaps

--- lua/russ/init.lua
+++ lua/russ/init.lua
@@ -1,1 +1,2 @@
+require("russ.remap")
 print("hello, world!")
---
+++ lua/russ/remap.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+
+
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {noremap = true})
+
+
+
