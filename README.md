cdc819bec1550ff9d43d7a5dcf2303a3ea870417
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2023-07-10 22:17:35 -0700

    Update README

--- README.md
+++ README.md
@@ -6,4 +6,21 @@
 
 ## Installing on a new system
 
+### 1. Install packer. 
+    The current command as of July 2023 is the following: 
+```
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+```
 
+However, it would be a good idea to go to `https://github.com/wbthomason/packer.nvim` and check out the current installation instructions
+
+### 2. Install GHCUP
+Source: `https://www.haskell.org/ghcup/`
+Command: 
+```
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+```
+Ensure that you type 'Y' when asked if you want the haskell language server.
+
+### 3. Start nvim and run :PackerSync
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/russ/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/russ/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/russ/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/russ/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/russ/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/home/russ/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/russ/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/russ/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/russ/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/russ/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,72 +76,72 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/Users/russ/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/russ/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/Users/russ/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/russ/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   harpoon = {
     loaded = true,
-    path = "/Users/russ/.local/share/nvim/site/pack/packer/start/harpoon",
+    path = "/home/russ/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/ThePrimeagen/harpoon"
   },
   ["lsp-zero.nvim"] = {
     loaded = true,
-    path = "/Users/russ/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
+    path = "/home/russ/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/Users/russ/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/home/russ/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/Users/russ/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/home/russ/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["monokai.nvim"] = {
     loaded = true,
-    path = "/Users/russ/.local/share/nvim/site/pack/packer/start/monokai.nvim",
+    path = "/home/russ/.local/share/nvim/site/pack/packer/start/monokai.nvim",
     url = "https://github.com/tanvirtin/monokai.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/Users/russ/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/russ/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/Users/russ/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/russ/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/Users/russ/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/russ/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/Users/russ/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/russ/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/Users/russ/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/russ/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/Users/russ/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/russ/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/Users/russ/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    path = "/home/russ/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   }
 }
