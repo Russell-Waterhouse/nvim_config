153cdd96bf67a99748ee327ef27312168070603d
Russell Waterhouse <russell@ruboss.com>
2023-10-19 10:08:30 -0700

    remove harpoon

--- lua/russ/packer.lua
+++ lua/russ/packer.lua
@@ -16,8 +16,6 @@ return require('packer').startup(function(use)
 
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
-	use('ThePrimeagen/harpoon')
-
 	use('tpope/vim-fugitive')
 
     use {
