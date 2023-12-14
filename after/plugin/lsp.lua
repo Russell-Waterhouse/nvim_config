92b6f14319fef57727a8ef523ce69f1b24bcff80
Russell Waterhouse <russell@ruboss.com>
2023-12-13 22:46:19 -0800

    remove hls from guaranteed install

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,7 +4,6 @@ lsp.preset('recommended')
 
 lsp.ensure_installed({
 	'rust_analyzer',
-    'hls',
     'eslint',
     'tsserver',
 })
--- lua/russ/remap.lua
+++ lua/russ/remap.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-
 -- jk to enter normal mode
 vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {noremap = true})
 
@@ -25,3 +24,4 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 -- copy to system clipboard with <Leader>y
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
