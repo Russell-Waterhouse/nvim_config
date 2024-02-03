61e820ebafa4bdf633d49a11f1cb0c4ff1e18640
Russell Waterhouse <russell@ruboss.com>
2024-02-03 15:21:26 -0800

    format status bar

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -61,19 +61,19 @@ require('lualine').setup {
     }
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
+    lualine_a = {'mode'}, -- NORMAL, INSERT, etc.
+    lualine_b = {'branch', 'diff', 'diagnostics'}, -- git branch
+    lualine_c = {'filename'}, -- filename
+    lualine_x = {},-- blank
+    lualine_y = {'encoding', 'fileformat', 'filetype'},
     lualine_z = {'location'}
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
     lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
+    lualine_x = {},
+    lualine_y = {'location'},
     lualine_z = {}
   },
   tabline = {},
