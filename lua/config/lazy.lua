c54ff0bf2842f77209a332caa96c36664f0d37ee
Russell Waterhouse <russell@ruboss.com>
2024-02-03 15:10:53 -0800

    redo config with lazy.nvim

---
+++ lua/config/lazy.lua
@@ -0,0 +1,83 @@
+-- require keymaps
+require('config.remap')
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+
+require("lazy").setup({
+	{
+		-- Treesitter: Better highlighting
+		{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+
+		-- Monokai theme
+		{ "tanvirtin/monokai.nvim" },
+
+		-- LSP support
+		{ "williamboman/mason.nvim" },
+
+		-- Bottom status bar
+		{ 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
+
+	},
+})
+
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+require('monokai').setup {}
+require("mason").setup()
+
+require('lualine').setup {
+  options = {
+    theme = molokai,
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
---
+++ lua/config/remap.lua
@@ -0,0 +1,30 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- jk to enter normal mode
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
+
+-- capital W saves too
+vim.keymap.set("n", "W", "w")
+
+-- Capital J doesn't fly to the right anymore
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- stay centered on screen when CTRL-D or CTRL-U
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- stay centered on screen while searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- replace with <Leader>s
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- copy to system clipboard with <Leader>y
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.cmd[[set rnu]]
+vim.cmd[[set nu]]
+
