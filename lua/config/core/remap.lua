ef0d3d6a3f5ea69e4059fa43329bc5a4756eb9e4
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2024-02-27 08:57:36 -0800

    add spellcheck keybinding

--- lua/config/core/remap.lua
+++ lua/config/core/remap.lua
@@ -41,6 +41,9 @@ keymap.set("n", "<leader>Y", [["+Y]])
 -- clear highlighting
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 
+-- turn on spellcheck (cs for check spelling)
+keymap.set("n", "<leader>cs", ":setlocal spell spelllang=en_us<CR>")
+
 -- window management
 keymap.set("n", "<leader>V", "<C-w>v") -- leader V split vertically
 keymap.set("n", "<leader>H", "<C-w>s") -- leader H split horizontally
